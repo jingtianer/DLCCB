fe0fb8bc684fcb3bf72de6b63ad7e635e7284a6e
jingtianer <jingtian@bupt.edu.cn>
2023-01-10 23:17:15 +0800

    修改脚本

--- net/mine.sh
+++ net/mine.sh
@@ -5,5 +5,5 @@ PORT=$(($2+30308))
 
 echo geth --datadir node$NODE --port $PORT --bootnodes $ENODE --networkid 12345 --unlock 0x$(cat node$NODE/keystore/UTC--* | awk '{split($0, arr, "\""); print arr[4]}') --password node1/password.txt --authrpc.port $AUTHRPCPORT
 
-geth --datadir node$NODE --port $PORT --bootnodes $ENODE --networkid 12345 --unlock 0x$(cat node$NODE/keystore/UTC--* | awk '{split($0, arr, "\""); print arr[4]}') --password node1/password.txt --authrpc.port $AUTHRPCPORT --mine < miner.txt
+geth --datadir node$NODE --port $PORT --bootnodes $ENODE --networkid 12345 --unlock 0x$(cat node$NODE/keystore/UTC--* | awk '{split($0, arr, "\""); print arr[4]}') --password node1/password.txt --authrpc.port $AUTHRPCPORT --mine
 
